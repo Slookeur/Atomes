f13d2e7cf378f7a5085c43e657bf2b7e928b262e
Slookeur <sebastien.leroux@ipcms.unistra.fr>
2024-03-11 15:58:43 +0100

    Atomes sources: lun. 11 mars 2024 15:58:43 CET

--- atomes-doxygen/run-doxygen.sh
+++ atomes-doxygen/run-doxygen.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 if [ $USER == 'leroux' ]; then
-  sed 's/ROOTDIR/\/data/g' atomes-doxygen.init > atomes-doxygen.cfg
+  sed 's/ROOTDIR/\/data/g' atomes-doxygen.init > ../src/atomes-doxygen.cfg
 else
-  sed 's/ROOTDIR/\/home\/slookeur/g' atomes-doxygen.init > atomes-doxygen.cfg
+  sed 's/ROOTDIR/\/home\/slookeur/g' atomes-doxygen.init > ../src/atomes-doxygen.cfg
 fi
 
+cp atomes-doxygen.md ../src/
+cd ../src
 doxygen atomes-doxygen.cfg
+rm -f atomes-doxgyen.*
